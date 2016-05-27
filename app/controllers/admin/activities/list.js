8cadefdb96806b3e723c31b2a1e0147077429318
Jacob Shwartz <jshwartz@gmail.com>
2016-05-27 08:32:13 -0700

    ui changes to admin activities dash

--- app/controllers/admin/activities/list.js
+++ app/controllers/admin/activities/list.js
@@ -1,16 +1,67 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
+  sortBy: 'titleAsc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      'zoneDesc': 'zone:desc,title:asc',
+      'zoneAsc': 'zone:asc,title:asc',
+      'catagoryDesc': 'catagory:desc,title:asc',
+      'catagoryAsc': 'catagory:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedActivities: Ember.computed.sort('matchingActivities', 'sortProperties'),
+  newActivityActive: false,
+  title: null,
+  searchTerm: '',
+  matchingActivities: Ember.computed('model.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    var model = this.get('model');
+    return model.filter(function(model) {
+      return model.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+
   actions: {
-    deleteActivity: function(activity) {
-      var addressId = activity.get('address').get('id'),
-          address = this.store.findRecord('address', addressId),
-          confirmDelete = window.confirm("Yo, you sure about this??");
-      if (confirmDelete) {
-        debugger;
-        activity.destroyRecord();
-        address.destroyRecord();
+    setSortingTitle() {
+      var currentSortBy = this.get('sortBy');
+      if (currentSortBy === 'titleAsc') {
+        this.set('sortBy', 'titleDesc');
+      } else {
+        this.set('sortBy', 'titleAsc');
+      }
+    },
+    setSortingZone() {
+      var currentSortBy = this.get('sortBy');
+      if (currentSortBy === 'zoneAsc') {
+        this.set('sortBy', 'zoneDesc');
+      } else {
+        this.set('sortBy', 'zoneAsc');
+      }
+    },
+    setSortingCatagory() {
+      var currentSortBy = this.get('sortBy');
+      if (currentSortBy === 'catagoryAsc') {
+        this.set('sortBy', 'catagoryDesc');
+      } else {
+        this.set('sortBy', 'catagoryAsc');
+      }
+    },
+    SetNewActivityActive() {
+      var newActivityActive = this.get('newActivityActive')
+      if (newActivityActive) {
+        this.set('newActivityActive', false);
+      } else {
+        this.set('newActivityActive', true);
       }
     },
   }
+
 });
--- app/routes/admin/activities/list.js
+++ app/routes/admin/activities/list.js
@@ -1,5 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  model: function() {
+    return this.store.findAll('activity');
+  },
+
+  actions: {
+    createActivity: function() {
+      var controller = this.get('controller');
+
+      var address = this.store.createRecord('address', controller.getProperties('address1', 'city', 'state', 'zip'));
+      var activity = this.store.createRecord('activity', controller.getProperties('title', 'catagory', 'rating', 'tileDetails', 'zone', 'duration', 'price', 'description'));
+      address.save().then(function() {
+        address.set('activity', activity);
+        activity.set('address', address);
+        address.save();
+        activity.save();
+
+      });
+      this.transitionTo('admin.activities.activity.meta', activity );
+    },
+    deleteActivity: function(activity) {
+      var controller = this.get('controller'),
+          addressId = activity.get('address').get('id'),
+
+          confirmDelete = window.confirm("Yo, you sure about this??");
+      if (confirmDelete) {
+        controller.store.findRecord('address', addressId).then(function(address) {
+          address.destroyRecord();
+          activity.destroyRecord();
+        });
+
+      }
+    },
+  }
+
 });

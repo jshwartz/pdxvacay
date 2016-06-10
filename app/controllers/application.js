8861cc58e60b6b3b19277660d146fe9abc0f381b
Jacob Shwartz <jshwartz@gmail.com>
2016-06-10 09:52:56 -0700

    modify bootstrap nav bar css

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
+
 export default Ember.Controller.extend({
+  expandNav: false,
+  actions: {
+    toggleNav: function() {
+      this.toggleProperty('expandNav');
+    }
+  }
 });
